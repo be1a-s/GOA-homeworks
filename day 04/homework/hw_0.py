92b6366854edd0fa8ed8a187bf99763126f8d1ec
be1a-s <beqaabuladze2009@gmail.com>
2025-02-19 18:19:28 +0400

    second commit

---
+++ day 04/homework/hw_0.py
@@ -0,0 +1,14 @@
+#შექმენით მინიმუმ 5 ცვლადი, რომლებშიც შეინახავთ თქვენი ოჯახის წევრების სახელებს (მაგალითად: mother: "Tamo"), შემდეგ კი დაბეჭდავთ ერთ დიდ წინადადებაში (მაგალითად print("my mother is" + " " + mother))
+
+mother = "irma"
+
+me = "beqa"
+
+father = "tamazi"
+
+sister_1 = "maia"
+
+sister_2 = "nino"
+
+print("i am " + me + " this is my family " + " my mother " + mother +  "," + "father "  + father + "," " sisters " + sister_1 + " and " + sister_2)
+
