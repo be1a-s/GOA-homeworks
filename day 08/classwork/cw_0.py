3a6b8c787309972281ebbb366118cf8b7d76426d
be1a-s <beqaabuladze2009@gmail.com>
2025-03-04 22:58:23 +0400

    second commit

---
+++ day 08/classwork/cw_0.py
@@ -0,0 +1,7 @@
+"""შექმენით პროგრამა, რომელშიც მომხმარებელს შემოატანინებთ სახელსა და გვარს, შემდეგ კი დაუბეჭდავთ ერთად"""
+
+name = input("enter your name:")
+
+surname = input("enter your surname:")
+
+print("hello " + name + surname)
