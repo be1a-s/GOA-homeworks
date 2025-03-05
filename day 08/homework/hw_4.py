c676ae639faa3fdb5323b0da2283894c10e5e1dc
be1a-s <beqaabuladze2009@gmail.com>
2025-03-05 18:07:52 +0400

    second commit

---
+++ day 08/homework/hw_4.py
@@ -0,0 +1,12 @@
+""" დაწერეთ პითონის კოდი რომელიც მომხმარებელს input-ის საშვალებით შეეკითხება და ცვლადში შეინახავს შემდეგ ინფორმაციას:"""
+
+
+name = input("enter your name: ")
+surname = input("enter your surname: ")
+age = input("enter your age:  ")
+location = input("enter your location: ")
+occupation = input("enter your occupation: ")
+hobby = input("enter your hobby: ")
+
+print(name + " " +surname + " is " + age + " years old, livs in " + location + ", works as a " + occupation + ", and enjoys " + hobby )
+
---
+++ day 08/homework/hw_5.py
@@ -0,0 +1,43 @@
+"""5) შექმენით 10 string ტიპის და 10 ინტეჯერ ტიპის ცვლადი, გააკეთეთ 5 კონკატინაციისა (სტრინგების შეერთების) და 5 მათემატიკური ჯამის მაგალითი"""
+
+#შევქმენი ათი სხვადასხვა მნიშვნელობის და სახელის მქონე string ტიპის ცვლადი
+name_1 = "beqa"
+surname_1 = "abuladze"
+
+name_2 = "tamazi"
+surname_2 = "abuladze"
+
+name_3 = "irma"
+surname_3 = "kekelidze"
+
+name_4 = "maia"
+surname_4 = "abuladze"
+
+name_5 = "nino"
+surname_5 = "abuladze"
+
+#print-ის საშვალებით შევასრულე ხუთი კონკატინაციის მაგალითი
+print(name_1 + surname_1)
+print(name_2 + surname_2)
+print(name_3 + surname_3)
+print(name_4 + surname_4)
+print(name_5 + surname_5)
+
+#შევქმენი ათი სხვადასხვა მნიშვნელობის და სახელის მქონე integer ტიპის ცვლადი
+num_1 = 10
+num_2 = 20
+num_3 = 30
+num_4 = 40
+num_5 = 50
+num_6 = 60
+num_7 = 70
+num_8 = 80
+num_9 = 90
+num_10 = 100
+
+#print-ის საშვალებით შევასრულე ხუთი მათემატიკური მაგალითი
+print(num_1 + num_2)
+print(num_3 + num_4)
+print(num_5 + num_6)
+print(num_7 + num_8)
+print(num_9 + num_10)
