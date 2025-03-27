30028b2a558348e7011691a89e2c73ecca0a565e
be1a-s <beqaabuladze2009@gmail.com>
2025-03-27 09:07:36 +0400

    second commit

---
+++ day 14/homework/hw_1.py
@@ -0,0 +1,3 @@
+"""controlflow არის კომპიუტერის დინების მართვა რომელიც შედგება სამი ნაწილისგან: sequences,iterations და selection-ის გან
+ sequences ნიშნავს კოდის თანმიმდევრობით დაწერას,iterations კოდის გამეორებას ხოლო selection კოდის არჩევას.
+"""
