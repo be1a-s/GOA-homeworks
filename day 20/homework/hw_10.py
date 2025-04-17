b8d43bb5df1e24db1ab171a4ef152716a8af2e1e
be1a-s <beqaabuladze2009@gmail.com>
2025-04-18 00:35:01 +0400

    second commit

---
+++ day 20/homework/hw_10.py
@@ -0,0 +1,29 @@
+"""7) მომხმარებელს შემოაყვანინეთ წინადადება, შემდეგ for ციკლისა და პირობითი განცხადებების საშვალებით დაბეჭდეთ ჯერ ხმოვნების, შემდეგ კი თანხმოვნების რაოდენობა (ხმოვნებად ჩათვალეთ სიმბოლოები: a, e, i, o, u ხოლო სხვა ყველა თანხმოვნად)"""
+
+text = input("enter your text: ")
+
+xmovani = ""
+
+for i in text:
+    if i == "a":
+        xmovani += i
+    elif i == "e":
+        xmovani += i
+    elif i == "i":
+        xmovani += i
+    elif i == "o":
+        xmovani += i
+    elif i == "u":
+        xmovani += i
+    elif i == "A":
+        xmovani += i
+    elif i == "E":
+        xmovani += i
+    elif i == "I":
+        xmovani += i
+    elif i == "O":
+        xmovani += i
+    elif i == "U":
+        xmovani += i
+    
+print(len(xmovani))
